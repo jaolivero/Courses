07fb1e07175ec6973c9d619750fa9fc2c3c9084a
joel olivero <joelolivero@outlook.com>
2024-02-25 19:49:37 -0500

    REST API built route for getting all courses.

---
+++ src/main/java/com/example/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+package com.example.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "AWS", "Amazon"),
+                new Course(2, "Azure", "Microsoft")
+        );
+    }
+
+
+
+}
